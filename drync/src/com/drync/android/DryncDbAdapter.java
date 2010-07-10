13567f7425f1b385e3a6b4b0a1d1401634530a11
U-Tsunami-Vista\Michael Brindamour <Michael Brindamour@Tsunami-Vista.(none)>
2010-07-10 11:14:53 -0400

    use different dbs for free vs pro allowing full side-by-side existance. fix an illegalstateexception caused by not closing the cursor used to count corks.

--- drync/src/com/drync/android/DryncDbAdapter.java
+++ drync/src/com/drync/android/DryncDbAdapter.java
@@ -55,7 +55,7 @@ public class DryncDbAdapter
     
     private static final String DATABASE_PRO_NAME = "drync";
     private static final String DATABASE_FREE_NAME = "dryncfree";
-    private static String DATABASE_NAME = DATABASE_PRO_NAME;
+    private static String DATABASE_NAME = DATABASE_FREE_NAME;
     private static final String DATABASE_TABLE = "corks";
     private static final int DATABASE_VERSION = 7;
 
@@ -106,7 +106,7 @@ public class DryncDbAdapter
     	if (DryncUtils.isFreeMode())
     		DATABASE_NAME = DATABASE_FREE_NAME;
     	else
-    		DATABASE_NAME = DATABASE_PRO_NAME;
+    		DATABASE_NAME = DATABASE_FREE_NAME;
     	
         DBHelper = new DatabaseHelper(context);
     }
@@ -438,7 +438,9 @@ public class DryncDbAdapter
                 null, 
                 null);
     	
-    	return cur.getCount();
+    	int count = cur.getCount();
+    	cur.close();
+    	return count;
     }
     
     public List<Cork> getAllCorks(boolean includePendingDeletes) 
